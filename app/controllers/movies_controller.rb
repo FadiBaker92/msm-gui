037ca405331ca33391d3cc5bec7a8cae2c5521b5
FADI BAKER <133148112+FadiBaker92@users.noreply.github.com>
2024-06-17 04:24:24 +0000

    adds update#movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,39 @@
 class MoviesController < ApplicationController
+
+  def update
+    m_id = params.fetch("the_id")
+    matching_record = Movie.where({:id => m_id})
+    the_movie = matching_record.at(0)
+    the_movie.title = params.fetch("the_title")
+    the_movie.year = params.fetch("the_year")
+    the_movie.duration = params.fetch("the_duration")
+    the_movie.description = params.fetch("the_description")
+    the_movie.image = params.fetch("the_image")
+    the_movie.director_id = params.fetch("the_director_id")
+    the_movie.save
+    redirect_to("/movies/#{the_movie.id}")
+  end
+
+  def destroy
+    the_id = params.fetch("an_id")
+    matching_record = Movie.where({:id => the_id})
+    the_movie = matching_record.at(0)
+    the_movie.destroy
+    redirect_to("/movies")
+  end
+
+  def create 
+    m = Movie.new
+    m.title = params.fetch("the_title")
+    m.year = params.fetch("the_year")
+    m.duration = params.fetch("the_duration")
+    m.description = params.fetch("the_description")
+    m.image = params.fetch("the_image")
+    m.director_id = params.fetch("the_director_id")
+    m.save
+    redirect_to("/movies")
+  end
+
   def index
     matching_movies = Movie.all
     @list_of_movies = matching_movies.order({ :created_at => :desc })
@@ -15,42 +50,11 @@ class MoviesController < ApplicationController
     render({ :template => "movie_templates/show" })
   end
 
-  def create 
-    m = Movie.new
-    m.title = params.fetch("the_title")
-    m.year = params.fetch("the_year")
-    m.duration = params.fetch("the_duration")
-    m.description = params.fetch("the_description")
-    m.image = params.fetch("the_image")
-    m.director_id = params.fetch("the_director_id")
-    m.save
-    redirect_to("/movies")
-  end
 
 
-  def destroy
-    the_id = params.fetch("an_id")
-    matching_record = Movie.where({:id => the_id})
-    the_movie = matching_record.at(0)
-    the_movie.destroy
-    redirect_to("/movies")
-  end
 
-  def update
-    m_id = params.fetch("the_id")
-    matching_record = Movie.where({:id => m_id})
-    the_movie = matching_record.at(0)
-    the_movie = Movie.new
-    the_movie.title = params.fetch("the_title")
-    the_movie.year = params.fetch("the_year")
-    the_movie.duration = params.fetch("the_duration")
-    the_movie.description = params.fetch("the_description")
-    the_movie.image = params.fetch("the_image")
-    the_movie.director_id = params.fetch("the_director_id")
-    the_movie.save
-    redirect_to("/movies/#{the_movie.id}")
 
-  end
+
 
 
 end
